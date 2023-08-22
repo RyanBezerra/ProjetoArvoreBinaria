c46bc246663fa3d013cf02f49f6692255ebaebad
Matheus Farias Porto Dos Anjos <114027492+Matheusota2k@users.noreply.github.com>
2023-08-22 08:11:17 -0300

    Update Sem Título1.cpp

--- Sem Título1.cpp
+++ Sem Título1.cpp
@@ -6,3 +6,24 @@ int main() {
     printf("Endereco de varint: %p", &varint);
     return 0;
 }
+
+
+#include <iostream>
+
+int main() {
+    // Tamanho do array
+    int tamanho = 5;
+
+    // Alocar um array de inteiros
+    int *array = new int[tamanho];
+
+    // Imprimir os endereços dos ponteiros
+    for (int i = 0; i < tamanho; i++) {
+        std::cout << "Endereco de array[" << i << "] = " << &array[i] << std::endl;
+    }
+
+    // Liberar a memória alocada para o array
+    delete[] array;
+
+    return 0;
+}
